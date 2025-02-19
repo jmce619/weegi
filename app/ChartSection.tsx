749c51a8412eac32ae97768e304f92493ed4895b
Jamie M. <jmcelhiney619@gmail.com>
2025-02-19 10:55:59 -0500

    format graph 1

--- app/ChartSection.tsx
+++ app/ChartSection.tsx
@@ -57,7 +57,7 @@ export default function ChartSection() {
 
         </div>
         <p className="mt-1 text-center text-xs font-semibold text-black opacity-75">
-          Median Family Income vs. Healthcare Insurance Stock Prices
+          Median Family Income vs. Healthcare Insurance Stock Prices ( % Change YoY)
           </p>
       </div>
 
